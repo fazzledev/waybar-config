fc34eeeefb85d1f40be015e9e330c967e234d970
Syed Fazil Basheer <fazil@fazn.co>
2025-09-06 09:43:29 +0530

    initial commit

---
+++ scripts/mouse_battery.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DEVICE="/org/freedesktop/UPower/devices/battery_hidpp_battery_0"
+BATTERY=$(upower -i "$DEVICE" | awk '/percentage/ { print $2 }')
+
+# Output JSON so Waybar can parse it
+echo "{\"text\": \"🖱️\", \"tooltip\": \"Mouse Battery: $BATTERY\"}"
