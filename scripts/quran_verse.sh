cf21a1fd42b715596fdf6a1a2429fb012b34f1ea
Syed Fazil Basheer <fazil@fazn.co>
2025-09-06 10:48:49 +0530

    feat: Configure and fix quran_verse.sh Waybar module

---
+++ scripts/quran_verse.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "{\"text\": \"Whatتل...\", \"alt\": \"11:86\", \"tooltip\": \"What remains [lawful] from Allāh is best for you, if you would be believers. But I am not a guardian over you.\"}"
