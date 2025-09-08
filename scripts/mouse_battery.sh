7d23276ac84adb7705b75aea2c6e4a045d5c5517
Syed Fazil Basheer <fazil@fazn.co>
2025-09-08 07:12:54 +0530

    feat: update mouse battery icon
    - Replaced the mouse emoji with a Nerd Font icon for consistency.
    
    - Added a CSS class to the mouse battery module for styling.
    
    - Updated the stylesheet to apply the same style as other icons.

--- scripts/mouse_battery.sh
+++ scripts/mouse_battery.sh
@@ -4,4 +4,4 @@ DEVICE="/org/freedesktop/UPower/devices/battery_hidpp_battery_0"
 BATTERY=$(upower -i "$DEVICE" | awk '/percentage/ { print $2 }')
 
 # Output JSON so Waybar can parse it
-echo "{\"text\": \"🖱️\", \"tooltip\": \"Mouse Battery: $BATTERY\"}"
+echo "{\"text\": \"󰍽\", \"class\": \"mouse-battery\", \"tooltip\": \"Mouse Battery: $BATTERY\"}"
